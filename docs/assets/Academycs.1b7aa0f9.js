3194a4e015b0872b0d662990894a0df8ee8747b3
Jose Daniel <jossolpue@alum.us.es>
2022-12-07 17:39:06 +0100

    Why me page and header

---
+++ docs/assets/Academycs.1b7aa0f9.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index.b95ab34e.js";const t=e("<h1>Academycs Page</h1>");function c(){return t.cloneNode(!0)}export{c as default};
--- docs/assets/Academycs.87ce8d30.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index.52730ee4.js";const t=e("<h1>Academycs Page</h1>");function c(){return t.cloneNode(!0)}export{c as default};
