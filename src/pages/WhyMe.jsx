97d889fc172be1b685216826c76f1f7f37dba52e
Jose Daniel <jossolpue@alum.us.es>
2022-12-07 20:20:14 +0100

    Revert "test8"
    This reverts commit 941edaa6a571dbc88ff1f0398e1c47a1755585eb.

--- src/pages/WhyMe.jsx
+++ src/pages/WhyMe.jsx
@@ -5,7 +5,7 @@ function WhyMe() {
     <Container>
       <Row>
         <Image style={{ "max-height": "800px", "margin-top": "25px" }}
-          src=""
+          src="https://raw.githubusercontent.com/DGEU-2022/alimanalc-jossolpue/269bb2c258bd3277e208dc2cf64f9f594960b437/src/assets/MyAddedValue.svg"
           rounded
         />
       </Row>
